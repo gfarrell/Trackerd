17c005e57e40a92fca761a7733951c15e4e21806
Gideon Farrell <me@gideonfarrell.co.uk>
2012-08-12 21:34:32 +0100

    Created basic app.js file for the main initialising scripts, needs proper implementation

---
+++ js/app.js
@@ -0,0 +1,16 @@
+/*
+    Trackerd - Helpful location tracking around cities
+    --------------------------------------------------
+
+    @file       app.js
+    @package    Trackerd
+    @author     Gideon Farrell <me@gideonfarrell.co.uk>
+ */
+
+define(['Collections/Places'], function() {
+    return {
+        init: function() {
+            return;
+        }
+    };
+});
