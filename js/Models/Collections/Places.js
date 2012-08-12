e4688cb3caf64676985845d92ece6a0f27704531
Gideon Farrell <me@gideonfarrell.co.uk>
2012-08-12 23:58:57 +0100

    Added Storage options to Collection

--- js/Models/Collections/Places.js
+++ js/Models/Collections/Places.js
@@ -3,14 +3,15 @@
     ------------------
 
     @provides   Places
-    @requires   libs/Backbone, Models/Place, Core/Sync
+    @requires   libs/Backbone, Models/Place, Core/Store, Core/Sync
     @file       Places.js
     @package    Trackerd/Models/Collections
     @author     Gideon Farrell <me@gideonfarrell.co.uk>
  */
 
-define(['Backbone', 'Models/Place', 'Core/Sync'], function(Backbone, Place) {
+define(['Backbone', 'Models/Place', 'Core/Store', 'Core/Sync'], function(Backbone, Store, Place) {
     return Backbone.Collection.extend({
-        model: Place
+        model: Place,
+        localStorage: new Store('Trackerd.Places')
     });
 });
