3f7bd0bc7098f40604555bdd110a95813c534b00
Gideon Farrell <me@gideonfarrell.co.uk>
2012-08-12 23:32:50 +0100

    Created synchronisation functionality from http://documentcloud.github.com/backbone/docs/backbone-localstorage.html

--- js/Core/Sync.js
+++ js/Core/Sync.js
@@ -9,6 +9,56 @@
     @author     Gideon Farrell <me@gideonfarrell.co.uk>
  */
 
-define(['Backbone', 'Core/LocalStorage', 'Core/String.extend'], function(Backbone, LocalStorage) {
-    
+define(['Backbone', 'Core/String.extend'], function(Backbone) {
+    var Store = function(name) {
+      this.name = name;
+      var store = localStorage.getItem(this.name);
+      this.data = (store && JSON.parse(store)) || {};
+    };
+
+    _.extend(Store.prototype, {
+      save: function() {
+        localStorage.setItem(this.name, JSON.encode(this.data));
+      },
+      create: function(model) {
+        if (!model.id) model.id = model.attributes.id = String.UUID();
+        this.data[model.id] = model;
+        this.save();
+        return model;
+      },
+      update: function(model) {
+        this.data[model.id] = model;
+        this.save();
+        return model;
+      },
+      find: function(model) {
+        return this.data[model.id];
+      },
+      findAll: function() {
+        return _.values(this.data);
+      },
+      destroy: function(model) {
+        delete this.data[model.id];
+        this.save();
+        return model;
+      }
+    });
+
+    Backbone.sync = function(method, model, options) {
+        var resp,
+            store = model.localStorage || model.collection.localStorage;
+
+        switch (method) {
+            case "read":    resp = model.id ? store.find(model) : store.findAll(); break;
+            case "create":  resp = store.create(model);                            break;
+            case "update":  resp = store.update(model);                            break;
+            case "delete":  resp = store.destroy(model);                           break;
+        }
+
+        if (resp) {
+            options.success(resp);
+        } else {
+            options.error("Record not found");
+        }
+    };
 });
