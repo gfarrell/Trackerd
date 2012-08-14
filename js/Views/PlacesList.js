68b56056b688699e5f203001a8f613deffe9c74d
Gideon Farrell <me@gideonfarrell.co.uk>
2012-08-14 03:45:52 +0100

    Started a class for displaying a list of places

---
+++ js/Views/PlacesList.js
@@ -0,0 +1,35 @@
+/*
+    PlacesListView
+    --------------
+
+    @requires   Backbone, Collections/Places, Views/PlaceRow
+    @file       PlacesListView.js
+    @package    Trackerd/Views
+    @author     Gideon Farrell <me@gideonfarrell.co.uk>
+ */
+
+define(
+    ['Backbone','Collections/Places','Views/PlaceRow'],
+    function(Backbone, Places, PlaceRow) {
+        return Backbone.View.extend({
+            tagName: 'ul',
+            className: 'PlacesList',
+
+            initialize: function() {
+                this._rows = [];
+                Places.on('all', this.render.bind(this));
+                Places.fetch();
+            },
+
+            render: function() {
+                Places.each(function(pl) {
+                    if(!instanceOf(this._rows[pl.cid], PlaceRow)) {
+                        var row = new PlaceRow({model: pl});
+                        this._rows[pl.cid] = row;
+                        row.$el.appendTo(this.$el);
+                    }
+                }, this);
+            }
+        });
+    }
+);
