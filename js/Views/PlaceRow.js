3863e9d3be245c0e91edec2e4837eb135fcbb941
Gideon Farrell <me@gideonfarrell.co.uk>
2012-08-19 22:50:03 +0100

    Added edit events and event aggregator integration

--- js/Views/PlaceRow.js
+++ js/Views/PlaceRow.js
@@ -2,43 +2,51 @@
     PlaceRowView
     --------------
 
-    @requires   Backbone, Place
     @file       PlaceRowView.js
     @package    Trackerd/Views
     @author     Gideon Farrell <me@gideonfarrell.co.uk>
  */
 
 define(
-    ['Backbone','Models/Place'],
-    function(Backbone, Places, Place) {
+    ['Backbone','Models/Place','text!Templates/PlaceRow.html'],
+    function(Backbone, Place, row_html) {
         return Backbone.View.extend({
             tagName: 'li',
             className: 'PlaceRow',
 
-            initialize: function() {
+            events: {
+                'click': 'edit'
+            },
+
+            initialize: function(options) {
+                this.__nc = options.__nc;
+
                 this.model.on('change', this.render, this);
                 this.model.on('destroy', this.remove, this);
 
                 this.$el.attr('data-id', this.model.cid);
 
-                this.$note = $('<span class="note"></span>');
-                this.$tags = $('<span class="tags"></span>');
-                this.$dist = $('<span class="distance"></span>');
-
-                this.$el.append(this.$note, this.$tags, this.$dist);
+                this.Template = _.template(row_html);
 
                 this.render();
             },
 
             render: function() {
-                this.$note.html(this.model.get('note'));
-                this.$tags.html(this.model.get('tags').join(', '));
-
-                this.renderDistance();
+                this.$el.empty();
+                this.$el.append(this.Template({
+                    note: this.model.get('note'),
+                    tags: this.model.get('tags'),
+                    distance: this.model.getDistance().round(2)
+                }));
+                this.delegateEvents();
             },
 
             renderDistance: function() {
-                this.$dist.html(this.model.getDistance().round(2)+'km');
+                this.$el.find('.distance').html(this.model.getDistance().round(2)+' km');
+            },
+
+            edit: function() {
+                this.__nc.trigger('editPlace', this.model.cid);
             }
         });
     }
