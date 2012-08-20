6e4d9dbdadaa74982d9b477cab8e2c46ba3bbf8d
Gideon Farrell <me@gideonfarrell.co.uk>
2012-08-20 02:05:38 +0100

    Rejigged render so that all els are created in the initialisation stage and they are just updated in render()

--- js/Views/PlaceRow.js
+++ js/Views/PlaceRow.js
@@ -11,39 +11,47 @@ define(
     ['Backbone','Models/Place','text!Templates/PlaceRow.html'],
     function(Backbone, Place, row_html) {
         return Backbone.View.extend({
-            tagName: 'li',
+            tagName: 'li',                                  // Each Row is an LI inside a UL
             className: 'PlaceRow container',
 
             events: {
-                'click .control.edit':   'edit',
-                'click .control.delete': 'confirmDelete'
+                'click .control.edit':   'edit',            // Controls for edit ...
+                'click .control.delete': 'confirmDelete'    // and delete (brings up a confirmation dialogue)
             },
 
             initialize: function(options) {
-                this.__nc = options.__nc;
+                this.__nc = options.__nc;                                   // Store the event aggregator
 
-                this.model.on('change', this.render, this);
-                this.model.on('destroy', this.remove, this);
+                this.__nc.on('locationUpdate', this.renderDistance, this);  // Whenever the current location is updated, re-render the distance
 
-                this.$el.attr('data-id', this.model.cid);
+                this.model.on('change', this.render, this);                 // Update the row on model change
+                this.model.on('destroy', this.remove, this);                // And delete the row when the model is deleted
 
-                this.Template = _.template(row_html);
+                this.$el.attr('data-id', this.model.cid);                   // Set a data-attribute, not currently used but may one day come in handy
 
-                this.render();
+                this.Template = _.template(row_html);                       // Store a template function which will generate the HTML
+
+                this.$el.append(this.Template({                             // Append the output of the templater
+                    note: this.model.get('note'),
+                    tags: this.model.get('tags'),
+                    distance: this.model.getDistance().round(2)             // More decimal places than two is a bit pointless
+                }));
+
+                this.els = {                                                // Store a load of shortcuts to the parts which need updating
+                    '$note': this.$el.find('.note'),
+                    '$tags': this.$el.find('.tags'),
+                    '$dist': this.$el.find('.distance')
+                };
             },
 
             render: function() {
-                this.$el.empty();
-                this.$el.append(this.Template({
-                    note: this.model.get('note'),
-                    tags: this.model.get('tags'),
-                    distance: this.model.getDistance().round(2)
-                }));
-                this.delegateEvents();
+                this.els.$note.html(this.model.get('note'));
+                this.els.$tags.html(this.model.get('tags'));
+                this.els.$dist.html(this.model.getDistance().round(2)+' km');
             },
 
             renderDistance: function() {
-                this.$el.find('.distance').html(this.model.getDistance().round(2)+' km');
+                this.els.$dist.html(this.model.getDistance().round(2)+' km');
             },
 
             edit: function() {
